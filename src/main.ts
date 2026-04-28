95e295c461852091095890c3da80ac5cc55b0dfd
aoshisen <aoshisen.23@gmail.com>
2026-04-28 23:58:14 +0800

    feat: 自动播放

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { compact } from "lodash-es";
 const app = document.getElementById("app") as HTMLCanvasElement;
 const next = document.getElementById('next')! as HTMLButtonElement;
+const play = document.getElementById('play')! as HTMLButtonElement;
 enum Status {
   Dead,
   Live,
@@ -17,11 +18,7 @@ const base_rule = (item: Item, item_neighbors: (Item | null)[]) => {
   return rule_engine[item.status][live_neighbors_count]
 }
 
-const DIRECTIONS = [
-  [-1, -1], [0, -1], [1, -1],
-  [-1, 0], [1, 0],
-  [-1, 1], [0, 1], [1, 1]
-];
+  ;
 
 class Board<T extends Item> {
   current_board: T[][] = [];
@@ -30,12 +27,21 @@ class Board<T extends Item> {
   rows: number;
   colors_map = ["#161616", "white"];
   next_board: T[][] = [];
+  is_playing: boolean;
+  timer: number | null = null;
+  DURATION = 200;
+  DIRECTIONS = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0], [1, 0],
+    [-1, 1], [0, 1], [1, 1]
+  ];
   constructor(public readonly width: number, public readonly height: number, public readonly itemSize: number, public readonly node: HTMLCanvasElement) {
     this.ctx = node.getContext('2d')!;
     this.cols = Math.floor(this.width / itemSize);
     this.rows = Math.floor(this.height / itemSize);
     this.current_board = Array.from({ length: this.cols }, () => Array(this.rows).fill(null));
     this.next_board = Array.from({ length: this.cols }, () => Array(this.rows).fill(null));
+    this.is_playing = false;
     this.node.addEventListener('click', this.canvasHandler.bind(this));
     this.loop((x, y) => {
       this.current_board[y][x] = { x, y, status: Status.Dead } as T;
@@ -59,8 +65,21 @@ class Board<T extends Item> {
       }
     }
   }
+  play() {
+    this.is_playing = true;
+    this.timer = setInterval(() => {
+      this.next()
+    }, this.DURATION)
+  }
+  pause() {
+    if (this.timer) {
+      clearInterval(this.timer)
+    }
+    this.timer = null;
+    this.is_playing = false;
+  }
   getNeighbors(item: T) {
-    return DIRECTIONS.map(([dx, dy]) => {
+    return this.DIRECTIONS.map(([dx, dy]) => {
       const x = item.x + dx;
       const y = item.y + dy;
       return this.getItem(x, y);
@@ -94,3 +113,12 @@ class Board<T extends Item> {
 const board = new Board(app.width, app.height, 10, app);
 board.reRender();
 next.addEventListener("click", board.next.bind(board))
+play.addEventListener("click", () => {
+  if (board.is_playing) {
+    board.pause()
+    play.innerText = "play"
+  } else {
+    board.play();
+    play.innerText = "pause"
+  }
+})
